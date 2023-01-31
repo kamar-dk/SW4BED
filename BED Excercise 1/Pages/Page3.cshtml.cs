957bfa03797cb55dc8aebfdb0be16c7423587b7b
KasperMartensen <tigriss.tiger@gmail.com>
2023-01-31 13:48:46 +0100

    Exercise 1 done

---
+++ BED Excercise 1/Pages/Page3.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Excercise_1.Pages
+{
+    public class Page3Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
