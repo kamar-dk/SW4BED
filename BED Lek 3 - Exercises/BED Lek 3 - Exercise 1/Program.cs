c4345f0c4391cbb7022243bf780198ec539c7c38
KasperMartensen <tigriss.tiger@gmail.com>
2023-02-14 13:59:06 +0100

    exercise done

--- BED Lek 3 - Exercises/BED Lek 3 - Exercise 1/Program.cs
+++ BED Lek 3 - Exercises/BED Lek 3 - Exercise 1/Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
 
+app.Use(async (context, next) =>
+{
+    await next();
+    if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
+    {
+        context.Request.Path = "/index.html";
+        await next();
+
+    }
+});
+
+
+
+app.MapFallbackToFile("/index.html");
 app.Run();
+//app.MapGet("/", () => "Hello World!");
